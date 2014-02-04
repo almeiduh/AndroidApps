297ce6fab37766e09c4bad3ac399af9124a770c4
Diogo Santos <diogosantosalmeida@hotmail.com>
2014-02-04 03:47:22 +0000

    Updated Crazy Tips App

--- CrazyTipCalc/gen/com/example/crazytipcalc/R.java
+++ CrazyTipCalc/gen/com/example/crazytipcalc/R.java
@@ -8,6 +8,9 @@
 package com.example.crazytipcalc;
 
 public final class R {
+    public static final class array {
+        public static final int problem_solving=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,30 +27,57 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int BillEditText=0x7f080000;
-        public static final int TipTextView=0x7f080002;
-        public static final int action_settings=0x7f080008;
-        public static final int billTextView=0x7f080001;
-        public static final int changeTipSeekBar=0x7f080007;
-        public static final int changeTipTextView=0x7f080006;
-        public static final int finalBillEditText=0x7f080004;
-        public static final int finalBilltextView=0x7f080005;
-        public static final int tipEditText=0x7f080003;
+        public static final int BillEditText=0x7f090000;
+        public static final int TipTextView=0x7f090002;
+        public static final int action_settings=0x7f090017;
+        public static final int availabilityRadioGroup=0x7f09000d;
+        public static final int availabilityTextView=0x7f09000c;
+        public static final int availableBadRadioButton=0x7f09000e;
+        public static final int availableGoodRadioButton=0x7f090010;
+        public static final int availableOkRadioButton=0x7f09000f;
+        public static final int billTextView=0x7f090001;
+        public static final int changeTipSeekBar=0x7f090006;
+        public static final int changeTipTextView=0x7f090007;
+        public static final int finalBillEditText=0x7f090004;
+        public static final int finalBilltextView=0x7f090005;
+        public static final int friendlyCheckBox=0x7f090009;
+        public static final int introTextView=0x7f090008;
+        public static final int optionCheckBox=0x7f09000a;
+        public static final int pauseButton=0x7f090015;
+        public static final int problemsSpinner=0x7f090011;
+        public static final int resetButton=0x7f090016;
+        public static final int specialsCheckBox=0x7f09000b;
+        public static final int startButton=0x7f090014;
+        public static final int timeWaitingChronometer=0x7f090013;
+        public static final int timeWaitingTextView=0x7f090012;
+        public static final int tipEditText=0x7f090003;
     }
     public static final class layout {
         public static final int activity_crazy_tip_calc=0x7f030000;
     }
     public static final class menu {
-        public static final int crazy_tip_calc=0x7f070000;
+        public static final int crazy_tip_calc=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int availability_bad_radio_button=0x7f05000e;
+        public static final int availability_good_radio_button=0x7f050010;
+        public static final int availability_ok_radio_button=0x7f05000f;
+        public static final int availability_text_view=0x7f05000d;
         public static final int bill_edit_text=0x7f050003;
         public static final int bill_text_view=0x7f050002;
         public static final int change_tip_text_view=0x7f050008;
         public static final int final_bill_edit_text=0x7f050007;
         public static final int final_text_view=0x7f050006;
+        public static final int intro_friendly_check_box=0x7f05000a;
+        public static final int intro_opinion_check_box=0x7f05000c;
+        public static final int intro_specials_check_box=0x7f05000b;
+        public static final int intro_text_view=0x7f050009;
+        public static final int pause_chrono_button=0x7f050013;
+        public static final int reset_chrono_button=0x7f050014;
+        public static final int start_chrono_button=0x7f050012;
+        public static final int time_waiting_text_view=0x7f050011;
         public static final int tip_edit_text=0x7f050005;
         public static final int tip_text_view=0x7f050004;
     }
@@ -73,10 +103,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
